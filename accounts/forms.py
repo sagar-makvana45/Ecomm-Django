0f8c8c79d60f437b3fca466284212aa8bb5f62e7
sagar-makvana45 <rajputsagar894@gmail.com>
2025-07-20 22:11:48 +0530

    fourth commit / register & login

---
+++ accounts/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['first_name','last_name','phone_number','username','email','password']
+
+    def __init__(self,*args,**kwargs):
+        super(RegistrationForm,self).__init__(*args,**kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter E-Mail'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Account.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is already registered.")
+        return email
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password Does Not Match.!"
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+from .models import Account
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
+            user.phone_number = phone_number
+            user.save()
+
+            return redirect('login')  # ✅ MUST RETURN SOMETHING HERE
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request,'accounts/login.html')
+
+def logout(request):
+    return
